406219d7db4bc9b4a48b8d503bbbc7ccabf4d1d5
janchua19 <jan.edward.chua@oocl.com>
2021-08-06 16:26:37 +0800

    [ParkingLot][Jan] feat: should_return_null_when_park_given_a_parking_lot_without_any_position_available

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -3,11 +3,16 @@ import java.util.Map;
 
 public class ParkingLot {
     private Map<ParkingTicket, Car> parkedPosition = new HashMap<>();
+    private int occupiedSlot = 0;
+    private final int maximumCapacity = 10;
 
     public ParkingTicket park(Car car) {
-        ParkingTicket parkingTicket = new ParkingTicket();
-        parkedPosition.put(parkingTicket, car);
-        return parkingTicket;
+        if(maximumCapacity < occupiedSlot) {
+            ParkingTicket parkingTicket = new ParkingTicket();
+            parkedPosition.put(parkingTicket, car);
+            return parkingTicket;
+        }
+        return null;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
