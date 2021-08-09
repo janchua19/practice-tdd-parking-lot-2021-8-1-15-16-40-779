e1c07052b5ca945d78c52af3df93966d46db7fc9
janchua19 <jan.edward.chua@oocl.com>
2021-08-09 22:28:15 +0800

    [Story5][Jan] refactor: implements should_return_the_right_car_with_each_ticket_when_fetch_the_car_twice_given_a_super_smart_parking_boy_who_manage_two_parking_lots_both_with_a_parked_car_and_two_parking_ticket

--- src/test/java/SuperSmartParkingBoyTest.java
+++ src/test/java/SuperSmartParkingBoyTest.java
@@ -46,4 +46,36 @@ public class SuperSmartParkingBoyTest{
         Car actualCar = parkingLot2.fetch(parkingTicket);
         assertEquals(car, actualCar);
     }
+
+    @Test
+    public void should_return_the_right_car_with_each_ticket_when_fetch_the_car_twice_given_a_super_smart_parking_boy_who_manage_two_parking_lots_both_with_a_parked_car_and_two_parking_ticket ()
+    {
+        //given
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        ParkingLot parkingLot1 = new ParkingLot(20);
+        //parkingLot1.park(new Car());
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        //parkingLot2.park(new Car());
+
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        ParkingTicket parkingTicket1 = smartParkingBoy.park(car1);
+        ParkingTicket parkingTicket2 = smartParkingBoy.park(car2);
+
+        //when
+        Car actualCar1 = smartParkingBoy.fetch(parkingTicket1);
+        Car actualCar2 = smartParkingBoy.fetch(parkingTicket2);
+
+        //then
+        assertEquals(car1, actualCar1);
+        assertEquals(car2, actualCar2);
+
+    }
 }
