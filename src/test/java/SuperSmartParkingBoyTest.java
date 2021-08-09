836ce49f56efacf2ebd397f449fa21de31a60483
janchua19 <jan.edward.chua@oocl.com>
2021-08-09 22:25:19 +0800

    [Story5][Jan] refactor: implements should_return_the_car_will_be_parked_to_the_second_parking_lot_when_park_given_a_super_smart_parking_boy_who_manages_two_parking_lots_first_is_full_and_second_with_available_position_but_parking_lot_2_has_larger_ratio_of_available_slot_and_a_car

--- src/test/java/SuperSmartParkingBoyTest.java
+++ src/test/java/SuperSmartParkingBoyTest.java
@@ -25,4 +25,25 @@ public class SuperSmartParkingBoyTest{
         Car actualCar = parkingLot1.fetch(parkingTicket);
         assertEquals(car, actualCar);
     }
+
+    @Test
+    public void should_return_the_car_will_be_parked_to_the_second_parking_lot_when_park_given_a_super_smart_parking_boy_who_manages_two_parking_lots_first_is_full_and_second_with_available_position_but_parking_lot_2_has_larger_ratio_of_available_slot_and_a_car ()
+    {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(20);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        parkingLot1.park(new Car());
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+
+        //when
+        ParkingTicket parkingTicket = superSmartParkingBoy.park(car);
+
+        //then
+        Car actualCar = parkingLot2.fetch(parkingTicket);
+        assertEquals(car, actualCar);
+    }
 }
